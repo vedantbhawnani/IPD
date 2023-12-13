db7a0f1f58706d5428751de9b86ad7eb728f10b4
vedantbhawnani <vedant.bhawnani@gmail.com>
2023-12-14 02:29:30 +0530

    Updated UI

--- lib/pages/Settings.dart
+++ lib/pages/Settings.dart
@@ -5,13 +5,14 @@ class Settings extends StatefulWidget {
   const Settings({Key? key}) : super(key: key);
 
   @override
-  State<Settings> createState() => _SettingsState();
+  State<Settings> createState() => _SettingsPageState();
 }
 
-class _SettingsState extends State<Settings> {
+class _SettingsPageState extends State<Settings> {
   bool _locationTrackingEnabled = true;
   int _mapType = 1; // 1: Standard, 2: Satellite, etc.
   String _navigationVoice = 'Female';
+  Color _mapThemeColor = Colors.blue;
 
   Future<void> _loadSettings() async {
     final prefs = await SharedPreferences.getInstance();
@@ -19,6 +20,8 @@ class _SettingsState extends State<Settings> {
       _locationTrackingEnabled = prefs.getBool('locationTracking') ?? true;
       _mapType = prefs.getInt('mapType') ?? 1;
       _navigationVoice = prefs.getString('navigationVoice') ?? 'Female';
+      _mapThemeColor =
+          Color(prefs.getInt('mapThemeColor') ?? Colors.blue.value);
     });
   }
 
@@ -27,6 +30,13 @@ class _SettingsState extends State<Settings> {
     prefs.setBool('locationTracking', _locationTrackingEnabled);
     prefs.setInt('mapType', _mapType);
     prefs.setString('navigationVoice', _navigationVoice);
+    prefs.setInt('mapThemeColor', _mapThemeColor.value);
+  }
+
+  void _clearData() async {
+    // Implement data clearing logic for search history or saved locations
+    // Show confirmation dialog and handle potential errors
+    // ...
   }
 
   @override
@@ -38,17 +48,14 @@ class _SettingsState extends State<Settings> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Settings'),
-      ),
+      appBar: AppBar(title: Text('Settings')),
       body: ListView(
         children: [
           SwitchListTile(
             title: Text('Enable Location Tracking'),
             value: _locationTrackingEnabled,
-            onChanged: (value) => setState(() {
-              _locationTrackingEnabled = value;
-            }),
+            onChanged: (value) =>
+                setState(() => _locationTrackingEnabled = value),
           ),
           DropdownButtonFormField(
             value: _mapType,
@@ -57,9 +64,7 @@ class _SettingsState extends State<Settings> {
               DropdownMenuItem(value: 2, child: Text('Satellite')),
               // ... other options
             ],
-            onChanged: (value) => setState(() {
-              _mapType = value as int;
-            }),
+            onChanged: (value) => setState(() => _mapType = value as int),
             decoration: InputDecoration(
               labelText: 'Map Type',
             ),
@@ -71,16 +76,41 @@ class _SettingsState extends State<Settings> {
               DropdownMenuItem(value: 'Male', child: Text('Male Voice')),
               // ... other options
             ],
-            onChanged: (value) => setState(() {
-              _navigationVoice = value as String;
-            }),
+            onChanged: (value) =>
+                setState(() => _navigationVoice = value as String),
             decoration: InputDecoration(
               labelText: 'Navigation Voice',
             ),
           ),
+          // Map Theme Preview Pane
+          Card(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text('Map Theme Preview'),
+            ),
+          ),
+          // Update this pane dynamically based on map type and theme color
+          // ...
           ElevatedButton(
             onPressed: _saveSettings,
             child: Text('Save Settings'),
+          ),
+          // Clear Data Button
+          ElevatedButton(
+            onPressed: _clearData,
+            child: Text('Clear Data'),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.redAccent,
+            ),
+          ),
+          // About Section
+          ExpansionTile(
+            title: Text('About'),
+            children: [
+              Text('App Version: 1.0.0'),
+              Text('Contact: support@yourapp.com'),
+              Text('Privacy Policy: https://yourapp.com/privacy'),
+            ],
           ),
         ],
       ),
